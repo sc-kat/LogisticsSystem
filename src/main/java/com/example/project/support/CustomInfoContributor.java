a44edddfd320bae92e7e45805a36e488a01d0209
alexandruprelipcian <prelipcian.alexandru@gmail.com>
2023-03-07 21:09:37 +0200

    actuator implemented (task 8)

---
+++ src/main/java/com/example/project/support/CustomInfoContributor.java
@@ -0,0 +1,24 @@
+package com.example.project.support;
+
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+
+@Component
+public class CustomInfoContributor implements InfoContributor {
+    final CurrentDateAndProfitService currentDateAndProfitService;
+
+    public CustomInfoContributor(CurrentDateAndProfitService currentDateAndProfitService) {
+        this.currentDateAndProfitService = currentDateAndProfitService;
+    }
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("current-date", currentDateAndProfitService.getCurrentDate());
+        builder.withDetail("overall-profit", currentDateAndProfitService.getProfit());
+    }
+}
